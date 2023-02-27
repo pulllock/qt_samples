efe3dd9b5295bce9ec10b201179ed8b8f00c9979
Pullock <pullocked@gmail.com>
2023-02-27 17:47:06 +0800

    widget

--- sample_2_1_widget/widget.cpp
+++ sample_2_1_widget/widget.cpp
@@ -1,15 +1,23 @@
 #include "widget.h"
+// Qt编译生成的与UI文件widget.ui对应的类定义文件
 #include "./ui_widget.h"
 
+// Widget类的实现
+
+// 构造函数：执行父类QWidget的构造函数，创建一个Ui::Widget类的对象ui，这个ui就是Widget的private部分定义的指针变量ui
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
+    // 构造函数初始化列表，用来初始化私有成员ui
     , ui(new Ui::Widget)
 {
+    // 执行了Ui::Widget类的setupUi()函数，这个函数实现窗口的生成与各种属性的设置、信号与槽的关联
     ui->setupUi(this);
 }
 
+// 析构函数
 Widget::~Widget()
 {
+    // 删除使用new创建的指针ui
     delete ui;
 }
 
--- sample_2_1_widget/widget.h
+++ sample_2_1_widget/widget.h
@@ -4,18 +4,25 @@
 #include <QWidget>
 
 QT_BEGIN_NAMESPACE
+// 声明一个命名空间Ui，包含一个类Widget，此Widget类是ui_widget.h文件里定义的类
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
 
+// Widget继承自QWidget
 class Widget : public QWidget
 {
+    // 宏Q_OBJECT，用来使用Qt的信号和槽机制
     Q_OBJECT
 
 public:
+    // 构造函数
     Widget(QWidget *parent = nullptr);
+
+    // 析构函数
     ~Widget();
 
 private:
+    // 这个指针指向了可视化设计界面，后面要访问界面上的组件都需要通过这个指针
     Ui::Widget *ui;
 };
 #endif // WIDGET_H
